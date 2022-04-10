8f9f929e75b213d75c7895a342f72ed993677016
Chirag Gokani <chiragokani@wireless-10-147-228-177.public.utexas.edu>
2022-04-10 11:54:41 -0500

    add Fourier acoustics code

--- 02 Code/rayleigh_1_water.m
+++ 02 Code/rayleigh_1_water.m
@@ -8,27 +8,27 @@ W_bar = waitbar(0,'Please wait...');
 % And also to define the plane from the piston to evaluate the rayleigh
 % intergral.
 f=1.092e9;%Frequency of Ultrasonic wave
-a=50e-3;%Radius of the circular piston
+b=50e-3;%Radius of the circular piston
 co=1481;%sound speed in water
 uo=1;% Particle velocity on the surface of the piston in m/s assume uniform across entire surface of piston
 M_size=101;% Meaning a linear line along the piston's diameter is M_size
-P_piston=zeros(M_size,M_size);%Initialize the coordinate mesh on the surface of the piston->Then fill in the position of each mesh grid
+P_plate=zeros(M_size,M_size);%Initialize the coordinate mesh on the surface of the piston->Then fill in the position of each mesh grid
 Count=0;
 %P_piston=gpuArray(P_piston);
-for i=1:length(P_piston(:,1))%Index Y
+for i=1:length(P_plate(:,1))%Index Y
     tic
-    for j=1:length(P_piston(1,:))%Index X
-        if ((i-(length(P_piston(:,1))+1)/2)^2+(j-(length(P_piston(:,1))+1)/2)^2)<=(length(P_piston(:,1))/2)^2% Draw a circle on the square matrix
-            P_piston(i,j)=1;
+    for j=1:length(P_plate(1,:))%Index X
+        if ((i-(length(P_plate(:,1))+1)/2)^2+(j-(length(P_plate(:,1))+1)/2)^2)<=(length(P_plate(:,1))/2)^2% Draw a circle on the square matrix
+            P_plate(i,j)=1;
             Count=Count+1;
         end
     end
     time=toc
-        waitbar_show(i,P_piston(:,1),time,W_bar)
+        waitbar_show(i,P_plate(:,1),time,W_bar)
     
 end
-imshow(P_piston)% show an imag of the piston
-S_piston=P_piston*pi*a^2/Count; %Assigna each mesh square with an area
+imshow(P_plate)% show an imag of the piston
+S_piston=P_plate*pi*b^2/Count; %Assigna each mesh square with an area
 D_P=10e-2;%Radius of the phase plate
 L=23.4e-3;%Distance between the piston and the phase plate
 P_size=101;%Number of partition points along the entire line
@@ -42,15 +42,15 @@ for i=1:length(Plate_P(:,1))%Index Y
     for j=1:length(Plate_P(1,:))%Index X
         Sum=0;
         if ((i-(length(Plate_P(:,1))+1)/2)^2+(j-(length(Plate_P(:,1))+1)/2)^2)<=(length(Plate_P(:,1))/2)^2%Draw a circle on the square matrix
-            for k=1:length(P_piston(:,1))
-                for l=1:length(P_piston(1,:))
-                    if P_piston(k,l)~=0
-                        Position_pistonY=(length(P_piston(:,1))/2)-k;%Positon in number of squares not converted to actual distance
-                        Position_pistonX=-1*((length(P_piston(1,:))/2)-l);%Positon in number of squares not converted to actual distance
+            for k=1:length(P_plate(:,1))
+                for l=1:length(P_plate(1,:))
+                    if P_plate(k,l)~=0
+                        Position_pistonY=(length(P_plate(:,1))/2)-k;%Positon in number of squares not converted to actual distance
+                        Position_pistonX=-1*((length(P_plate(1,:))/2)-l);%Positon in number of squares not converted to actual distance
                         Position_PlateY=(length(Plate_P(:,1))/2)-i;%Positon in number of squares not converted to actual distance
                         Position_PlateX=-1*((length(Plate_P(1,:))/2)-j);%Positon in number of squares not converted to actual distance
-                        Position_pistonY=Position_pistonY*a/(length(P_piston(:,1))/2);%Covert to Real Y coordinate of the point
-                        Position_pistonX=Position_pistonX*a/(length(P_piston(:,1))/2);%Covert to Real X coordinate of the point
+                        Position_pistonY=Position_pistonY*b/(length(P_plate(:,1))/2);%Covert to Real Y coordinate of the point
+                        Position_pistonX=Position_pistonX*b/(length(P_plate(:,1))/2);%Covert to Real X coordinate of the point
                         Position_PlateY=Position_PlateY*D_P/(length(Plate_P(:,1))/2);%Covert to Real Y coordinate of the point
                         Position_PlateX=Position_PlateX*D_P/(length(Plate_P(:,1))/2);%Covert to Real X coordinate of the point
                         dist=sqrt((Position_PlateX-Position_pistonX)^2+(Position_PlateY-Position_pistonY)^2+L^2);%Calculate the distance between the phase plate...
@@ -76,3 +76,78 @@ pcolor(abs(Plate_P))
 %The next step should be to define the necessary phase on each of the point on the
 %phase plate to yield a acoustics vortex beam First better to sanity check
 %the current result
+
+phi = atan(Position_PlateY/Position_PlateX);
+b=75e-3;%Radius of the phase plate
+%piecewise definition of phase plate:
+
+for sector = 1:12 % = l = angular momentum number (topological charge)
+    Phi = sector*phi;
+end
+
+%{
+M_size=101;% Meaning a linear line along the piston's diameter is M_size
+P_plate=zeros(M_size,M_size);%Initialize the coordinate mesh on the surface of the piston->Then fill in the position of each mesh grid
+Count=0;
+%P_piston=gpuArray(P_piston);
+for i=1:length(P_plate(:,1))%Index Y
+    tic
+    for j=1:length(P_plate(1,:))%Index X
+        if ((i-(length(P_plate(:,1))+1)/2)^2+(j-(length(P_plate(:,1))+1)/2)^2)<=(length(P_plate(:,1))/2)^2% Draw a circle on the square matrix
+            P_plate(i,j)=1;
+            Count=Count+1;
+        end
+    end
+    time=toc
+        waitbar_show(i,P_plate(:,1),time,W_bar)
+    
+end
+
+imshow(P_plate)% show an imag of the piston
+S_piston=P_plate*pi*b^2/Count; %Assigna each mesh square with an area
+D_P=10e-2;%Radius of the phase plate
+L=23.4e-3;%Distance between the piston and the phase plate
+P_size=101;%Number of partition points along the entire line
+Plate_P=zeros(P_size,P_size);%Innitialize the pressure pattern
+Count=0;
+%}
+
+%{
+for i=1:length(Plate_P(:,1))%Index Y
+    tic
+    
+    for j=1:length(Plate_P(1,:))%Index X
+        Sum=0;
+        if ((i-(length(Plate_P(:,1))+1)/2)^2+(j-(length(Plate_P(:,1))+1)/2)^2)<=(length(Plate_P(:,1))/2)^2%Draw a circle on the square matrix
+            for k=1:length(P_piston(:,1))
+                for l=1:length(P_piston(1,:))
+                    if P_piston(k,l)~=0
+                        Position_pistonY=(length(P_piston(:,1))/2)-k;%Positon in number of squares not converted to actual distance
+                        Position_pistonX=-1*((length(P_piston(1,:))/2)-l);%Positon in number of squares not converted to actual distance
+                        Position_PlateY=(length(Plate_P(:,1))/2)-i;%Positon in number of squares not converted to actual distance
+                        Position_PlateX=-1*((length(Plate_P(1,:))/2)-j);%Positon in number of squares not converted to actual distance
+                        Position_pistonY=Position_pistonY*a/(length(P_piston(:,1))/2);%Covert to Real Y coordinate of the point
+                        Position_pistonX=Position_pistonX*a/(length(P_piston(:,1))/2);%Covert to Real X coordinate of the point
+                        Position_PlateY=Position_PlateY*D_P/(length(Plate_P(:,1))/2);%Covert to Real Y coordinate of the point
+                        Position_PlateX=Position_PlateX*D_P/(length(Plate_P(:,1))/2);%Covert to Real X coordinate of the point
+                        dist=sqrt((Position_PlateX-Position_pistonX)^2+(Position_PlateY-Position_pistonY)^2+L^2);%Calculate the distance between the phase plate...
+                        %and the circular piston
+                        
+                        Sum=Sum+1/(2*pi)*pressureresultS_piston(k,l)*exp(-sqrt(-1)*2*pi*f*dist/co)/(2*pi*dist);%Perform rayleigh integral on each mesh point on the piston
+                    end
+                end
+            end
+            Plate_P(i,j)=Sum;%Assign rayleigh intergral result onto each point on the phase plate
+            Count=Count+1;%Record how many partitions there are for the phase plate
+        end    
+    end
+        time=toc
+        waitbar_show(i,Plate_P(:,1),time,W_bar)
+end
+
+figure(1)
+pcolor(angle(Plate_P))
+figure(2)
+pcolor(abs(Plate_P))
+%}
+
